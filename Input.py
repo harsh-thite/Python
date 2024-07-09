ca3e8ac075d572407a2af89822f7f23bdb62277c
Harsh Thite <harshthite2003@gmail.com>
2024-07-10 00:46:21 +0530

    How to take input from User.

--- Input.py
+++ Input.py
@@ -1,2 +1,17 @@
-name = input("enter you age: ")
-print("you entered", name)
+# name = input("enter you age: ")
+# print("you entered", name)
+# int ( input()) #int
+# float ( input()) #float
+
+# int("5")
+# val = int(input("enter some value: "))
+# print(type(val), val) 
+
+#Taking Name, Age, and Marks as input from user.
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks: ")
+
+print("welcome", name)
+print("age =", age)
+print("marks =", marks)
