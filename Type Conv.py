52b092eaed19ee3635970ef3e7cbb3a826f8e4a1
Harsh Thite <harshthite2003@gmail.com>
2024-07-10 00:00:13 +0530

    Learned about Type Conversion and Type Casting in Python.

---
+++ Type Conv.py
@@ -0,0 +1,21 @@
+# Type Conversion
+a = 3
+b = 5.34
+
+sum = a + b #3 = 3.0
+print(sum) 
+
+
+#Type Casting
+a = int("2") #Only works if there is a valid value
+b = 5.23
+
+print(type(a))
+print(a + b)
+
+
+#Conversion of Int to String
+a = 4.23
+a = str(a)
+
+print(type(a))
